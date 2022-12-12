b98a1fd04ea5702b03505d7c0527e87a53068b96
Faris Rasyid <85282829+rasyid1003@users.noreply.github.com>
2022-12-12 19:23:05 +0700

    Add files via upload

---
+++ running_text.ino
@@ -0,0 +1,33 @@
+
+#include <LiquidCrystal_I2C.h>
+
+int totalColumns = 16;
+int totalRows = 2;
+
+LiquidCrystal_I2C lcd(0x27, totalColumns, totalRows);  
+
+String staticMessage = "Faris Rasyid";
+String scrollingMessage = "Selamat Datang.";
+
+void scrollMessage(int row, String message, int delayTime, int totalColumns) {
+  for (int i=0; i < totalColumns; i++) {
+    message = " " + message;  
+  } 
+  message = message + " "; 
+  for (int position = 0; position < message.length(); position++) {
+    lcd.setCursor(0, row);
+    lcd.print(message.substring(position, position + totalColumns));
+    delay(delayTime);
+  }
+}
+
+void setup(){
+  lcd.init();                    
+  lcd.backlight();
+}
+
+void loop(){
+  lcd.setCursor(0, 0);
+  lcd.print(staticMessage);
+  scrollMessage(1, scrollingMessage, 250, totalColumns);
+}
